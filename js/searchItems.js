ae9263f8d3cea660db99c32baac6e4e409608e4d
Horea Tiff <107192045+horeatiff@users.noreply.github.com>
2022-08-23 19:36:52 +0300

    Add files via upload

--- js/searchItems.js
+++ js/searchItems.js
@@ -3,7 +3,7 @@ function searchImg() {
   const imageAnchor = document.getElementsByTagName("a");
 
   for (let i = 0; i < imageAnchor.length; i++) {
-    if (!imageAnchor[i].innerHTML.toLowerCase().includes(input)) {
+    if (!imageAnchor[i].dataset.caption.toLowerCase().includes(input)) {
       imageAnchor[i].style.display = "none";
     } else {
       imageAnchor[i].style.display = "block";
